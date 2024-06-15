7d72c49a87c6e6742dff1889b85e1969dec5328a
Kayla Lee <kl2jmx@virginia.edu>
2024-06-15 19:41:28 -0400

    commit

---
+++ ComputerHardwareHistograms.py
@@ -0,0 +1,6 @@
+# Using Panda to manipulate data into a binnable form to make histograms of the dataset
+
+import pandas as pd 
+
+df = pd.read_excel('C:/Users/leeka/Downloads/PythonBadge/ComputerSurveyData.xlsx')
+print(df.head())
