bd68f786900da7ce5f7c9fa0436933e12affcedf
Kayla Lee <kl2jmx@virginia.edu>
2024-06-15 20:45:31 -0400

    commit

--- ComputerHardwareHistograms.py
+++ ComputerHardwareHistograms.py
@@ -1,6 +1,19 @@
 # Using Panda to manipulate data into a binnable form to make histograms of the dataset
 
 import pandas as pd 
+import matplotlib.pyplot as plt
 
-df = pd.read_excel('C:/Users/leeka/Downloads/PythonBadge/ComputerSurveyData.xlsx')
-print(df.head())
+dataFrame = pd.read_excel('C:/Users/leeka/Downloads/PythonBadge/ComputerSurveyData.xlsx')
+print(dataFrame.head())
+
+column_to_bin = 'CPU Cycle Rate (in GHz)'
+bins = [0, 1.8, 2.8, 3.5, 4.05, 4.7]
+
+# Creating histogram
+plt.figure(figsize = (8, 6))
+plt.hist(dataFrame['CPU Cycle Rate (in GHz)'], bins = bins)
+plt.xlabel('CPU Cycle Rate (GHz)')
+plt.ylabel('Number of CPUs')
+plt.title('CPU Cycle Rates')
+plt.grid(True)
+plt.show()
